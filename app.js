0ff6f7bc0c2b09e0642e3207b4894b2a467a7721
Xavier Carty <xaviercarty.student@careerdevs.com>
2018-12-18 00:57:46 +0000

    used Array.isArray , Array.from, Array.from(titles).forEach

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
-const titles = document.getElementsByClassName('title');
-
+var titles = document.getElementsByClassName("title");
 console.log(Array.isArray(titles));
 console.log(Array.isArray(Array.from(titles)));
 
-Array.from(titles).forEach(function(title){
-  console.log(title);
-});
+
+ Array.from(titles).forEach(function(item){
+     console.log(item);
+ }) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const titles = document.getElementsByClassName('title');
+
+// console.log(Array.isArray(titles));
+// console.log(Array.isArray(Array.from(titles)));
+
+// Array.from(titles).forEach(function(title){
+//   console.log(title);
+// });
