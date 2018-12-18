5d5b4a43eb45fdcfd6a4a36b70c29209d9ec5333
Xavier Carty <xaviercarty.student@careerdevs.com>
2018-12-18 23:15:53 +0000

    DOM append to innerHTML

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
-const wmf = console.log(document.querySelector('#book-list li:nth-child(2) .name'));
-//console.log(wmf)
-var books = console.log(document.querySelector('#book-list li .name'));
-//console.log(books)
+var books = document.querySelectorAll("#book-list li.name");
+Array.from(books).forEach(function(book){
+  book.textContent += '(book title")';
+});
 
-books = document.querySelectorAll('#book-list li .name');
-//console.log(books)
-Array.from(books).forEach(function(book){ console.log(book)
-})
-
-
-// git add -A
-// git commit -m "book list, nth child .name" 
-// git checkout master
-// git merge version 4.0
-// git push 
+const booklist = document.querySelector("#book-list");
+//booklist.innerHTML = "<h2>Books and more books </h2>";
+booklist.innerHTML += "<p> This is how you add html<p>"; 
 
 
-// const wmf = document.querySelector('#book-list li:nth-child(2) .name');
-// console.log(wmf);
 
-// var books = document.querySelector('#book-list li .name');
-// console.log(books);
 
-// books = document.querySelectorAll('#book-list li .name');
-// console.log(books);
+
+
+
+
+
+
+
+
+// //const books = document.querySelectorAll('#book-list li .name');
 
 // Array.from(books).forEach(function(book){
-//   console.log(book);
+//   book.textContent += ' (Book title)';
 // });
+
+// const bookList = document.querySelector('#book-list');
+// bookList.innerHTML = '<h2>Books and more books...</h2>';
+// bookList.innerHTML += '<p>This is how you add HTML content</p>';
