884fac217b8513dfc1e87bd98967860807552faf
Xavier Carty <xaviercarty.student@careerdevs.com>
2018-12-18 01:38:51 +0000

    book list, nth child .name

--- app.js
+++ app.js
@@ -1,35 +1,30 @@
-var titles = document.getElementsByClassName("title");
-console.log(Array.isArray(titles));
-console.log(Array.isArray(Array.from(titles)));
+const wmf = console.log(document.querySelector('#book-list li:nth-child(2) .name'));
+//console.log(wmf)
+var books = console.log(document.querySelector('#book-list li .name'));
+//console.log(books)
+
+books = document.querySelectorAll('#book-list li .name');
+//console.log(books)
+Array.from(books).forEach(function(book){ console.log(book)
+})
 
 
- Array.from(titles).forEach(function(item){
-     console.log(item);
- }) 
+// git add -A
+// git commit -m "book list, nth child .name" 
+// git checkout master
+// git merge version 4.0
+// git push 
 
 
+// const wmf = document.querySelector('#book-list li:nth-child(2) .name');
+// console.log(wmf);
 
+// var books = document.querySelector('#book-list li .name');
+// console.log(books);
 
+// books = document.querySelectorAll('#book-list li .name');
+// console.log(books);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const titles = document.getElementsByClassName('title');
-
-// console.log(Array.isArray(titles));
-// console.log(Array.isArray(Array.from(titles)));
-
-// Array.from(titles).forEach(function(title){
-//   console.log(title);
+// Array.from(books).forEach(function(book){
+//   console.log(book);
 // });
