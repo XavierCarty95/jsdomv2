c2030cbba258be8d51cdaa738bb079b4b9912db5
Xavier Carty <xaviercarty.student@careerdevs.com>
2018-12-19 01:48:08 +0000

    prevent default fire actions

--- app.js
+++ app.js
@@ -1,35 +1,26 @@
-const list = document.querySelector("#book-list ul");
+const addform = document.forms["add-book"];
+addform.addEventListener('submit',function(e){
+  e.preventDefault();
+  const value = addform.querySelector("input[type='text']").value
+  console.log(value)
+  
+});
 
-list.addEventListener('click' , function(e) { 
-  if(e.target.className == 'delete') {
-    const li = e.target.parentElement; 
-    list.removeChild(li) 
+
+
+
+
+
+
+// const list = document.querySelector("#book-list ul");
+
+// list.addEventListener('click' , function(e) { 
+//   if(e.target.className == 'delete') {
+//     const li = e.target.parentElement; 
+//     list.removeChild(li) 
      
     
-  }
-})
-
-
-
-
-
-
-
-
-
-// var btns = document.querySelectorAll("#book-list .delete");
-// Array.from(btns).forEach(function(btn){ btn.addEventListener('click',function(e) { 
-  
-//   const li = e.target.parentElement;
-//   li.parentNode.removeChild(li)
-  
-//   });
-// });
-
-// const link = document.querySelector("#page-banner a");
-
-// link.addEventListener('click', function(e) { e.preventDefault();
-// console.log('navigation to' , e.target.textContent, 'was prevented')
+//   }
 // })
 
 
@@ -40,26 +31,20 @@ list.addEventListener('click' , function(e) {
 
 
 
+// // var btns = document.querySelectorAll("#book-list .delete");
+// // Array.from(btns).forEach(function(btn){ btn.addEventListener('click',function(e) { 
+  
+// //   const li = e.target.parentElement;
+// //   li.parentNode.removeChild(li)
+  
+// //   });
+// // });
 
+// // const link = document.querySelector("#page-banner a");
 
-
-
-
-
-
-
-// // const bookList = document.querySelector('#book-list');
-// // console.log("book-list next sibling is:", bookList.nextSibling);
-// // console.log("book-list next sibling is:", bookList.nextElementSibling);
-
-
-// // console.log("book-list previous sibling is:", bookList.previousSibling);
-// // console.log("book-list previous sibling is:", bookList.previousElementSibling);
-
-
-// // bookList.previousElementSibling.querySelector("p").innerHTML += "<br/> too cool for everyone else!"
-
-
+// // link.addEventListener('click', function(e) { e.preventDefault();
+// // console.log('navigation to' , e.target.textContent, 'was prevented')
+// // })
 
 
 
@@ -78,10 +63,39 @@ list.addEventListener('click' , function(e) {
 
 
 // // // const bookList = document.querySelector('#book-list');
+// // // console.log("book-list next sibling is:", bookList.nextSibling);
+// // // console.log("book-list next sibling is:", bookList.nextElementSibling);
 
-// // // console.log('#book-list next sibling:', bookList.nextSibling);
-// // // console.log('#book-list next element sibling:', bookList.nextElementSibling);
-// // // console.log('#book-list previous sibling:', bookList.previousSibling);
-// // // console.log('#book-list previous element sibling:', bookList.previousElementSibling);
 
-// // // bookList.previousElementSibling.querySelector('p').innerHTML += '<br />Too cool for everyone else!';
+// // // console.log("book-list previous sibling is:", bookList.previousSibling);
+// // // console.log("book-list previous sibling is:", bookList.previousElementSibling);
+
+
+// // // bookList.previousElementSibling.querySelector("p").innerHTML += "<br/> too cool for everyone else!"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // // // const bookList = document.querySelector('#book-list');
+
+// // // // console.log('#book-list next sibling:', bookList.nextSibling);
+// // // // console.log('#book-list next element sibling:', bookList.nextElementSibling);
+// // // // console.log('#book-list previous sibling:', bookList.previousSibling);
+// // // // console.log('#book-list previous element sibling:', bookList.previousElementSibling);
+
+// // // // bookList.previousElementSibling.querySelector('p').innerHTML += '<br />Too cool for everyone else!';
